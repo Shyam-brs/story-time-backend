a83b58e9c3d76fdd6e5d79d74c3a96ac2a85b246
Shyam sundhar Kunduru <123252647+shyamsundharkunduru-brs@users.noreply.github.com>
2024-05-31 12:34:21 +0530

    init commit

---
+++ src/utils/utils.js
@@ -0,0 +1,64 @@
+import ejs from "ejs";
+import nodemailer from "nodemailer";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const currentFilePath = import.meta.url;
+const currentDirectory = dirname(fileURLToPath(currentFilePath));
+
+console.log(currentDirectory);
+
+const mail = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: "shyam020524@gmail.com",
+    pass: "hggvjpshwigtmckk",
+  },
+});
+
+// send mail
+const sendEmailVerificationLink = async (email, token, name) => {
+  try {
+    const renderedContent = await ejs.renderFile(
+      `${currentDirectory}/../templates/confirm_email.ejs`,
+      { token, name }
+    );
+
+    const mailOptions = {
+      from: "shyam020524@gmail.com",
+      to: email,
+      subject: "Storytime - Email Confirmation",
+      html: renderedContent,
+    };
+
+    const verificationInfo = await mail.sendMail(mailOptions);
+    return verificationInfo;
+  } catch (error) {
+    return { error };
+  }
+};
+
+const sendPasswordResetLink = async (email, token, name) => {
+  try {
+    const renderedContent = await ejs.renderFile(
+      `${currentDirectory}/../templates/reset_password.ejs`,
+      { token, name }
+    );
+
+    const mailOptions = {
+      from: "shyam020524@gmail.com",
+      to: email,
+      subject: "Storytime - Password reset link",
+      html: renderedContent,
+    };
+
+    const verificationInfo = await mail.sendMail(mailOptions);
+    return verificationInfo;
+  } catch (error) {
+    return { error };
+  }
+};
+
+export { sendEmailVerificationLink, sendPasswordResetLink };
